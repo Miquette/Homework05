deac3075cac7e0865ea32f7c89dbbf000ecbc01d
Miquette <morren19@my.whitworth.edu>
2017-04-07 00:03:59 -0700

    The Good One
    fixed some needed changes on the code

--- SourceAccount05.cpp
+++ SourceAccount05.cpp
@@ -1,3 +1,4 @@
+//NEW AND IMPROVED ACCOUNT CLASS DISREGARD THE PREVIOUS ONE
 ///////////////////
 // Miquette Orren
 // CS 172
@@ -17,7 +18,7 @@ class Account
 private:
 	int id;
 	double balance;
-	const double annualInterestRate = 4.0;
+	const double annualInterestRate = 0.015;
 	string name;
 	vector <Transaction> transactions;
 public:
@@ -57,10 +58,7 @@ public:
 	{
 		return annualInterestRate;
 	}
-	void setannualinterestrate(double newannualrate)
-	{
-		annualInterestRate = newannualrate;
-	}
+	
 
 
 	double getMonthlyInterestRate()     //Couldn't seem to get this one to work. Maybe because it's a double? I kept trying Steve.getMonthlyInterestRate.
@@ -71,13 +69,17 @@ public:
 
 	double withdraw(int amount)
 	{
-		transactions.push_back(1);
-		return balance - amount;
+		transactions.push_back(-1);
+		int newbalance;
+		newbalance = balance - amount;
+		return newbalance;
 	}
 	double deposit(int amount)
 	{
 		transactions.push_back(1);
-		return balance + amount;
+		int newbalance;
+		newbalance = balance + amount;
+		return newbalance;
 	}
 
 };
@@ -107,9 +109,30 @@ public:
 
 int main()
 {
+	
+	Account George;
+	George.setid(1122);
+	George.setbalance(1000);
+	Transaction t;
 
+	cout << "Welcome to Miser Bank, George. Your account number is " << George.id << " and your current balance is " << George.balance << "." << endl;
+	cout << "The current and static interest rate is " << George.getannualInterestRate * 100 << "%." << endl;
+	cout << endl;
+	cout << "If you withdrew $5 from your account, you would be left with $" << George.withdraw(5) << "." << endl;
+	Transaction('W', 5, 1000, "withdrawal from account");
+	cout << George.withdraw(4) << endl;
+	Transaction('W', 4, George.withdraw(4), "deposit after withdrawal");
+	cout << George.withdraw(2) << endl;
+	Transaction('W', 2, George.withdraw(2));
+	cout << "If you simply deposited the $30 without withdrawing, you would have $" << George.deposit(30) << " in your account." << endl;
+	Transaction('D', 30, George.deposit(30), "deposit to account");
+	cout << George.deposit(40) << endl;
+	Transaction('D', 40, George.deposit(40), "deposit to account");
+	cout << George.deposit(50) << endl;;
+	Transaction('D', 50, George.deposit(50), "deposit into account");
+	cout << "The monthly interest rate for you, George, is " << (George.getannualInterestRate / 12) * 100 << "%." << endl;
 
-
+	cout << "The amount of transactions for you this month thus far is " << transactions.size() << endl;
 
 
 	return 0;
